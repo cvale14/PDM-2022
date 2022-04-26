3bfafd57eb7fee77d1b0c7f3ee1aeaeff94d117c
cvale14 <98445249+cvale14@users.noreply.github.com>
2022-04-26 14:17:04 -0500

    p5 assignment

--- Drawing with p5 Example 3/sketch.js
+++ Drawing with p5 Example 3/sketch.js
@@ -8,12 +8,21 @@ function draw() {
   var scalarX = 80;
   var scalarY = 40;
   arc(215, 200, 350, 350, 0, PI + QUARTER_PI * 3);
-  strokeWeight(5);
-point(84, 91);
-point(68, 19);
-point(21, 17);
-point(32, 91);
-strokeWeight(1);
+
+  noStroke()
+  fill(500, 85, 100)
+  square(425, 40, 350);
+  square(425, 10, 350, 20);
+
+  fill(255)
+  circle(700, 100, 130);
+  circle(500, 100, 130);
+  fill(0, 0, 250)
+  circle(700, 100, 100);
+  circle(500, 100, 100);
+
+
+
 
 
 }
