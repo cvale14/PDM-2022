9f62ed5613754c653af43633ab4b8fcf54372bf6
cvale14 <98445249+cvale14@users.noreply.github.com>
2022-04-26 10:51:47 -0500

    assignment updates

---
+++ Drawing with p5 Example 3/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 400);
+}
+
+function draw() {
+  background(0);
+  fill(400, 600, 50)
+  var scalarX = 80;
+  var scalarY = 40;
+  arc(215, 200, 350, 350, 0, PI + QUARTER_PI * 3);
+  strokeWeight(5);
+point(84, 91);
+point(68, 19);
+point(21, 17);
+point(32, 91);
+strokeWeight(1);
+
+
+}
